a1e09de1f0358eae7a552db9222fae5b024f2de2
Juan Carlos Alonso <juan.alonso@wizeline.com>
2022-05-11 17:54:38 -0500

    Enable Test 05

--- frontend/tests/todoist_tests.spec.js
+++ frontend/tests/todoist_tests.spec.js
@@ -74,26 +74,26 @@ test('Todoist Login 3rd Attempt', async ({ page }) => {
 });
 
 // Test 05 - Create a New Task
-//test('Create a New Task', async ({ page }) => {
-//  const todoistDev = new TodoistDevPage(page);
+test('Create a New Task', async ({ page }) => {
+  const todoistDev = new TodoistDevPage(page);
 
-//  console.log('Log In to Todoist...');
-//  await todoistDev.goto();
-//  await expect(todoistDev.page).toHaveTitle("Todoist: The to do list to organize work & life");
+  console.log('Log In to Todoist...');
+  await todoistDev.goto();
+  await expect(todoistDev.page).toHaveTitle("Todoist: The to do list to organize work & life");
 
-//  await todoistDev.gotoLoginPage();
-//  await expect(todoistDev.page).toHaveTitle("Log in to Todoist");
+  await todoistDev.gotoLoginPage();
+  await expect(todoistDev.page).toHaveTitle("Log in to Todoist");
 
-//  await todoistDev.goodCredentials();
-//  await todoistDev.loginButton();
-//  await expect(todoistDev.page).toHaveTitle("Today: Todoist");
-//  console.log('Log In Success!')
+  await todoistDev.goodCredentials();
+  await todoistDev.loginButton();
+  await expect(todoistDev.page).toHaveTitle("Today: Todoist");
+  console.log('Log In Success!')
 
-//  await page.click('text=Add a task');
-//  await todoistDev.createTask(1);
-//  console.log('Task created successfully!');
+  await page.click('text=Add a task');
+  await todoistDev.createTask(1);
+  console.log('Task created successfully!');
 
-//});
+});
 
 // Test 06 - Create 10 Tasks
 //test('Create 10 Tasks', async ({ page }) => {
